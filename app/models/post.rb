559ecb4d8277e172215f587cf6d0d5c0ce27d38c
annabush092 <annabush092@gmail.com>
2017-09-25 14:50:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+
+  class BuzzValidator < ActiveModel::Validator
+    def validate(record)
+      result = /Won't Believe|Secret|Guess|Top \d/i.match(record.title)
+      if !result
+        record.errors[:title] << "Missing key buzzwords in your title"
+      end
+    end
+  end
+
+  validates :title, presence: true, buzz: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+  validates_with BuzzValidator
+
 end
